ff2713e9e07cc5e5dc83fe7d0f8bfacc73e8c0dc
ajharul64 <ajharul.islam@barclays.com>
2024-05-21 12:46:56 +0100

    final edit

--- index.test.js
+++ index.test.js
@@ -8,6 +8,7 @@ const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
 const seedMusician = require("./seedData");
+const { timeStamp } = require('console');
 
 
 describe('./musicians endpoint', () => {
@@ -23,6 +24,24 @@ describe('./musicians endpoint', () => {
         expect(response.statusCode).toBe(200);
     })
     
+    // test("Testing musicians endpoint", async () => {
+    //     // Sends request to `/bakedGoods` endpoint
+    //     const response = await request(app).get("/musicians/:id");
+    //     expect(response.statusCode).toBe(200);
+    // })
+
+    test("should respond with correct data", async () => {
+        // Sends request to `/bakedGoods` endpoint
+        const response = await request(app).get("/musicians/1");
+        expect(response.statusCode).toBe(200);
+        const responseData = JSON.parse(response.text);
+        // Write expect tests here
+        expect(responseData).toEqual(expect.objectContaining({
+            "id": 1,
+            "name": "Mick Jagger",
+            "instrument": "Voice"
+          }))
+    })
 
 
 
--- src/app.js
+++ src/app.js
@@ -13,6 +13,34 @@ app.get('/musicians', async (req, res)=>{
 })
 
 
+app.get('/musicians/:id', async (req, res)=>{
+    const musician = await Musician.findByPk(req.params.id)
+        res.json(musician)
+    
+})
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post('/musicians/:id', async (req, res)=>{
+    const musician = await Musician.create(req.body)
+    res.json(musician)
+
+})
+
+app.put('/musicians/:id', async (req, res)=>{
+    const updatedmusician = await Musician.update(req.body, {where: {id: req.params.id}})
+    res.json(updatedmusician)
+
+})
+
+app.delete('/musicians/:id', async (req, res)=>{
+    const deletedmusician = await Musician.destroy({where: {id: req.params.id}})
+    res.json(deletedmusician)
+
+})
+
+
 
 
 
